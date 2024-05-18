e02448f6a3407a7e62da6bb504793f237d305667
smartmohan20 <dev.smartmohan@gmail.com~>
2024-05-18 18:21:09 +0530

    Implemented enpoint to get all users, changes for method name and some unnecessory code removed

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -5,39 +5,53 @@ import { User } from '../database/models/models.js';
 const strCurrFileUrl = import.meta.url
 
 // Controller to historical data
-export const create = async (req, res) => {
+export const createUser = async (req, res) => {
     try {
         let objRes = ISErrorRes;
         const { name, email, password, dob } = req.body;
 
-        try {
-            // Create user
-            const objUser = await User.create({
-                name,
-                email: email,
-                password,
-                dob
-            });
+        // Create user
+        const objUser = await User.create({
+            name,
+            email: email,
+            password,
+            dob
+        });
 
-            // Send response with status code
-            objRes = {
-                statusCode: 200,
-                message: 'User created successfully!',
-                data: objUser
-            };
-        } catch (error) {
-            objRes = {
-                statusCode: 400,
-                message: 'Failed to create user!',
-                data: {},
-                errors: [error.message]
-            };
-        }
+        // Send response with status code
+        objRes = {
+            statusCode: 200,
+            message: 'User created successfully!',
+            data: objUser
+        };
         
         // Send json response
         await sendJsonResponse(res, objRes);
     } catch (error) {
         console.error('Exception occured in "create"!', 'File: ', strCurrFileUrl, 'Error: ', error);
+        return sendJsonResponse(res, {...ISErrorRes, errors: [error.message]});
+    }
+};
+
+// Controller to get all users
+export const getAllUser = async (req, res) => {
+    try {
+        let objRes = ISErrorRes;
+
+        // Get all users
+        const arrUsers = await User.findAll();
+
+        // Send response with status code
+        objRes = {
+            statusCode: 200,
+            message: 'Users fetched successfully!',
+            data: arrUsers
+        };
+        
+        // Send json response
+        await sendJsonResponse(res, objRes);
+    } catch (error) {
+        console.error('Exception occured in "getAll"!', 'File: ', strCurrFileUrl, 'Error: ', error);
         return sendJsonResponse(res, {...ISErrorRes, errors: [error.message]});
     }
 };
--- src/routes/user/userRoutes.js
+++ src/routes/user/userRoutes.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import { validate as createVal } from '../../validators/user/create.js';
-import { create as createCon } from '../../controllers/user.js';
+import { createUser as createCon, getAllUser as getAllCon } from '../../controllers/user.js';
 
 const userRouter = express.Router();
 
 // Create user route
 userRouter.post('/', createVal, createCon);
+
+// Get all users route
+userRouter.get('/', getAllCon);
 
 export default userRouter;
