7a64176d6e92fb5702a0a6a68bdc0744efff9ffa
smartmohan20 <dev.smartmohan@gmail.com~>
2024-05-18 18:29:02 +0530

    Added condition for if user already exist

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -4,26 +4,72 @@ import { User } from '../database/models/models.js';
 
 const strCurrFileUrl = import.meta.url
 
+// Method to find user by email
+const findUserByEmail = async (email) => {
+    try {
+        let objRes = ISErrorRes;
+
+        // Find user by email
+        const objUser = await User.findOne({
+            where: {
+                email
+            }
+        });
+
+        if (objUser) {
+            // Successful response
+            objRes = {
+                statusCode: 200,
+                message: 'User found successfully!',
+                data: objUser
+            };
+        } else {
+            // Failure response
+            objRes = {
+                statusCode: 404,
+                message: 'User not found!',
+                data: {},
+                errors: []
+            };
+        }
+        
+        return objRes;
+    } catch (error) {
+        console.error('Exception occured in "findUserByEmail"!', 'File: ', strCurrFileUrl, 'Error: ', error);
+    }
+};
+
 // Controller to historical data
 export const createUser = async (req, res) => {
     try {
         let objRes = ISErrorRes;
         const { name, email, password, dob } = req.body;
 
-        // Create user
-        const objUser = await User.create({
-            name,
-            email: email,
-            password,
-            dob
-        });
+        const findUserRes = await findUserByEmail(email);
+        if (findUserRes?.statusCode === 200) {
+            // Failure response
+            objRes = {
+                statusCode: 409,
+                message: 'User already exists!',
+                data: {},
+                errors: []
+            };
+        } else {
+            // Create user
+            const objUser = await User.create({
+                name,
+                email: email,
+                password,
+                dob
+            });
 
-        // Send response with status code
-        objRes = {
-            statusCode: 200,
-            message: 'User created successfully!',
-            data: objUser
-        };
+            // Successful response
+            objRes = {
+                statusCode: 200,
+                message: 'User created successfully!',
+                data: objUser
+            };
+        }
         
         // Send json response
         await sendJsonResponse(res, objRes);
