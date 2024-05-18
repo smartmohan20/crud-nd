9eb282786aed798903724ef7221821ff2a6dfbc9
smartmohan20 <dev.smartmohan@gmail.com~>
2024-05-18 16:56:33 +0530

    Establised db connection and tested it

--- src/app.js
+++ src/app.js
@@ -3,8 +3,35 @@ import * as dotenv from 'dotenv';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 
+const strCurrFileUrl = import.meta.url
+
+// Test database connection and sync models
+const testAndSyncDB = async () => {
+   try {
+      const sequelize = (await import('./database/database.js')).default;
+      await import('./database/models/models.js');
+
+      // Test database connection
+      await sequelize.authenticate()
+         .then(() => {
+            console.log('Database connection established successfully.');
+         })
+         .catch((err) => {
+            console.warn('Unable to connect to the database:', err);
+         });
+
+      // Sync Sequelize models with the database
+      await sequelize.sync({ alter: true });
+   } catch (error) {
+      console.error('Exception occured in "testAndSyncDB"!', 'File: ', strCurrFileUrl, 'Error: ', error);
+   }
+};
+
 // Load environment variables from .env file
 dotenv.config();
+
+// Test and sync database
+await testAndSyncDB();
 
 // Create the Express app
 const app = express();
